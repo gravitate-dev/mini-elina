525c14a7d0a40136c4cda7b668b8bf8a5d23b864
Duke <16564906+gusrbehfdl@users.noreply.github.com>
2020-04-14 16:36:07 -0800

    Behavior Designer keeps throwing "ArgumentOutOfRangeException: Non-negative number required." errors, forcing me to keep recreate BT. So I'm going to save progress as oftern as I go.

---
+++ Assets/Duke/IsNull.cs
@@ -0,0 +1,16 @@
+﻿namespace BehaviorDesigner.Runtime.Tasks.Unity.UnityGameObject {
+    [TaskCategory("Unity/GameObject")]
+    [TaskDescription("Returns Success if the GameObject is null, otherwise Failure.")]
+    public class IsNull : Conditional {
+        [Tooltip("The GameObject that the task operates on. If null the task GameObject is used.")]
+        public SharedGameObject targetGameObject;
+
+        public override TaskStatus OnUpdate() {
+            return GetDefaultGameObject(targetGameObject.Value) == null ? TaskStatus.Success : TaskStatus.Failure;
+        }
+
+        public override void OnReset() {
+            targetGameObject = null;
+        }
+    }
+}
