fc8c058c9227b71cedac7de69802e004360cdcbc
Duke <16564906+gusrbehfdl@users.noreply.github.com>
2020-04-16 08:40:36 -0800

    Basic movement Move to attack range & return to spawn pos

---
+++ Assets/Duke/Behavior Designer/Action/GetPosNearTarget.cs
@@ -0,0 +1,28 @@
+﻿using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+namespace AI {
+    [TaskCategory("AStarPathfindingProject")]
+    [TaskDescription("Get a position near target position.")]
+    public class GetPosNearTarget : Action {
+        public SharedVector3 TargetPos;
+        public SharedVector2 RadiusRange;
+        public SharedVector2 AngleRange;
+        public SharedVector3 ReturnPos;
+
+        public override TaskStatus OnUpdate() {
+            if (TargetPos == null || RadiusRange == null || AngleRange == null) {
+                return TaskStatus.Failure;
+            }
+            else {
+                Vector3 dir = transform.position - TargetPos.Value;
+                dir.Normalize();
+                float randomAngle = Mathf.Clamp(Random.Range(AngleRange.Value.x, AngleRange.Value.y), -180f, 180f);
+                float randomRadius = Mathf.Abs(Random.Range(RadiusRange.Value.x, RadiusRange.Value.y));
+                ReturnPos.Value = TargetPos.Value + Quaternion.Euler(0, randomAngle, 0) * dir * randomRadius;
+                return TaskStatus.Success;
+            }
+        }
+    }
+}
---
+++ Assets/Duke/Behavior Designer/Action/MoveToPos.cs
@@ -0,0 +1,26 @@
+﻿using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using Pathfinding;
+
+namespace AI {
+    [TaskCategory("AStarPathfindingProject")]
+    [TaskDescription("Tells AI to move to a position.")]
+    public class MoveToPos : Action {
+        IAstarAI _ai;
+        public SharedVector3 Destination;
+
+        public override void OnAwake() {
+            _ai = gameObject.GetComponent<IAstarAI>();
+        }
+
+        public override TaskStatus OnUpdate() {
+            if (_ai == null || Destination == null) {
+                return TaskStatus.Failure;
+            }
+            else {
+                _ai.destination = Destination.Value;
+                return TaskStatus.Success;
+            }
+        }
+    }
+}
